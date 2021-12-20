ac0d54daac3cb6ab1f97b119dc5402c6e80908a1
Gregory Sims <briggybros.2@googlemail.com>
2021-12-20 20:45:01 +0000

    Remove unused useRef import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useMemo, useRef, useState} from 'react';
+import React, {useCallback, useEffect, useMemo, useState} from 'react';
 
 import PaletteView from './Components/PaletteView';
 import { AnalogousPalette, MonochromaticPalette, TetradicPalette } from './Palette/Palettes';
